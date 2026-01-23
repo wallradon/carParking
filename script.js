f34b121981ad595532455bfcae8bca8110d14787
Wallradon Promanan <wallradon@gmail.com>
2026-01-24 00:22:35 +0700

    เปลี่ยนทิศทางจอดรถ แยกซ้ายขวา

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function updateParking() {
-  const stoper = document.querySelectorAll(".car");
+  const stoperR = document.querySelectorAll(".carR");
+  const stoperL = document.querySelectorAll(".carL");
 
-  stoper.forEach(slot => {
+  stoperL.forEach(slot => {
+    const name = slot.getAttribute('name');
+    const haveCar = slot.querySelector('.carImg') != null;
+    if (haveCar) {
+      console.log(`Parking ${name} is Full`);
+    } else {
+      console.log(`Parking ${name} Is Empty`);
+    }
+  })
+  console.log("\n");
 
+  stoperR.forEach(slot => {
     const name = slot.getAttribute('name');
     const haveCar = slot.querySelector('.carImg') != null;
     if (haveCar) {
@@ -12,30 +23,65 @@ function updateParking() {
     }
   })
 }
-updateParking()
-const carimg = document.querySelectorAll(".car");
-carimg.forEach(carIsFull => {
-  carIsFull.addEventListener("click", function () {
 
-    const existingCar = this.querySelector('.carImg');
+updateParking();
 
-    if (existingCar == null) {
-      const newCar = document.createElement("img");
-      newCar.src = "img/car.png";
-      newCar.className = "carImg";
-      newCar.alt = "Car";
-      this.appendChild(newCar);
-      console.log(`Add car`);
-    } else {
-      existingCar.remove();
-      console.log(`Can not add car`);
-
-    }
+function addCar() {
+  const carimgL = document.querySelectorAll(".carL"); //เก็บข้อมูลจาก class = car
+  carimgL.forEach(carIsFull => {
+    carIsFull.addEventListener("click", function () {
+      const existingCar = this.querySelector('.carImg');
+      const spanstate = this.querySelector('span');
+      if (existingCar == null) {
+        const newCar = document.createElement("img");
+        newCar.src = "img/car.png";
+        newCar.className = "carImg";
+        newCar.alt = "Car";
+        this.appendChild(newCar);
+        //add class in span
+        if (spanstate) {
+          spanstate.classList.add("hidden-field");
+        }
+        console.log(`Add car`);
+      } else {
+        existingCar.remove();
+        console.log(`Can not add car`);
+        if (spanstate) {
+          spanstate.classList.remove("hidden-field");
+        }
+      }
+    });
   });
-});
+  const carimgR = document.querySelectorAll(".carR"); //เก็บข้อมูลจาก class = car
+  carimgR.forEach(carIsFull => {
+    carIsFull.addEventListener("click", function () {
+      const existingCar = this.querySelector('.carImg');
+      const spanstate = this.querySelector('span');
+      if (existingCar == null) {
+        const newCar = document.createElement("img");
+        newCar.src = "img/car R.png";
+        newCar.className = "carImg";
+        newCar.alt = "Car";
+        this.appendChild(newCar);
+        if (spanstate) {
+          spanstate.classList.add("hidden-field");
+        }
+        console.log(`Add car`);
+
+      } else {
+        existingCar.remove();
+        console.log(`Can not add car`);
+        if (spanstate) {
+          spanstate.classList.remove("hidden-field");
+        }
+      }
+    });
+  });
+}
+addCar()
 
 
-setInterval(() => {
-  updateParking();
-}, 5000); // สั่ง update ทุก 2 วิ
+// setInterval(() => {
+//   updateParking();
+// }, 2000); // สั่ง update ทุก 2 วิ
 
