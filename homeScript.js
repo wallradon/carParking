59c162855551972015fc7e4fe979882534889edd
Wallradon Promanan <wallradon@gmail.com>
2026-02-04 19:36:02 +0700

    makeNewFninHomejs

--- homeScript.js
+++ homeScript.js
@@ -1,6 +1,17 @@
+
 const modal = document.querySelector("#myModal");
 const closeModal = document.querySelector("#closeModal");
-
 modal.showModal();
 
-closeModal.addEventListener("click", function () { modal.close(); });
+closeModal.addEventListener("click", function (event) {
+
+  event.preventDefault();
+
+  modal.close();
+});
+
+modal.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    modal.close();
+  }
+});
